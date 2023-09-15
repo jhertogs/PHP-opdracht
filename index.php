9061f08ccc1165f46c047cdfed62e0a41807e829
Josse Hertogs <jossehertogs@gmail.com>
2023-09-15 10:17:31 +0200

    the selected cou try now stays

--- index.php
+++ index.php
@@ -6,23 +6,9 @@
     <title> PHP opdracht - vertaler</title>
 </head>
 <body>
-    <form method="post" action="">
-        Naam: <input type="text" name="naam" placeholder="Uw naam"required /><br/>
-        Land: 
-        <select name="land">
-            <option value="Keuze">Maak uw keuze</option>
-            <option value="NL">Nederland</option>
-            <option value="DE">Duitsland</option>
-            <option value="EN">Engeland</option>
-            <option value="FR">Frankrijk</option>
-            <option value="ES">Spanje</option>
-            <option value="IT">Italie</option>
-        </select>
-        <br/>
-        <input type="submit" name="submit" value="gegevens versturen"/>
-</form>
 <?php
-if(isset($_POST["submit"])){
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     $naam = $_POST["naam"];
     $land = $_POST["land"];
     if($land=='NL'){
@@ -51,6 +37,22 @@ if(isset($_POST["submit"])){
 }
 
 ?>
+    <form method="post" action="">
+        Naam: <input type="text" name="naam" placeholder="Uw naam" value="<?php if(isset($_POST["land"])){echo $_POST["naam"];}  ?>" required /><br/>
+        Land: 
+        <select name="land">
+            <option value="Keuze">Maak uw keuze</option>
+            <option value="NL" <?php if($land == "NL"){echo "selected";}?>>Nederland</option>
+            <option value="DE" <?php if($land == "DE"){echo "selected";}?>>Duitsland</option>
+            <option value="EN" <?php if($land == "EN"){echo "selected";}?>>Engeland</option>
+            <option value="FR" <?php if($land == "FR"){echo "selected";}?>>Frankrijk</option>
+            <option value="ES" <?php if($land == "ES"){echo "selected";}?>>Spanje</option>
+            <option value="IT" <?php if($land == "IT"){echo "selected";}?>>Italie</option>
+        </select>
+        <br/>
+        <input type="submit" name="submit" value="gegevens versturen"/>
+</form>
+
 </body>
 </html>
 
